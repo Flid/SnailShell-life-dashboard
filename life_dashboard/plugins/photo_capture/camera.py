10ba841a1022dbefc845769950e3e103ce818b48
Anton Kirilenko <anton.kirilenko@babylonhealth.com>
2018-03-19 21:14:18 +0000

    Camera works

--- life_dashboard/plugins/photo_capture/camera.py
+++ life_dashboard/plugins/photo_capture/camera.py
@@ -12,33 +12,37 @@ except ImportError:
 
 class RPiCamera(object):
     def __init__(self, preview=False):
-        self._cap = PiCamera()
-        self._cap.resolution = (640, 480)
-        self._cap.framerate = 1
-
+        self._cap = PiCamera(
+            framerate=2,
+            resolution=(640, 480),
+        )
         if preview:
             self._cap.start_preview()
-
         self._raw_capture = PiRGBArray(self._cap)
         time.sleep(0.1)  # Let it stabilize
 
-    def get_frame(self):
+    def get_frame(self, f='rgb'):
         self._raw_capture.truncate(0)
-        self._cap.capture(self._raw_capture, format='L')
+        self._cap.capture(self._raw_capture, format=f)
         return self._raw_capture.array
 
     def capture_continuous(self):
         frame_generator = self._cap.capture_continuous(
             self._raw_capture,
-            format='bgr',
+            format='rgb',
             use_video_port=True,
+            burst=False,
         )
-
         for frame in frame_generator:
             yield frame.array
             self._raw_capture.truncate(0)
+            # Setting framerate to 1 results in a black screen for some reason,
+            # 2 with 0.5 sec sleep here works fine.
+            time.sleep(0.5)
 
     def __del__(self):
+        self._raw_capture.close()
+        self._cap.close()
         del self._cap
 
 
